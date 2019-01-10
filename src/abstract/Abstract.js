caf8ef79fec5a1868c5dd514e13b00d227d7af4c
davis90 <davis_france@hotmail.com>
2019-01-10 09:50:20 +0100

    refactor(Abstract) : change conditions order in constructor

--- src/abstract/Abstract.js
+++ src/abstract/Abstract.js
@@ -2,15 +2,15 @@ class Abstract {
   /**
    * Abstract constructor. Abstract can't be instantiated
    * @class
-   * @throws {Error} Try to instantiate an abstract class
    * @throws {Error} Try to instantiate Abstract class
+   * @throws {Error} Try to instantiate an abstract class
    */
   constructor() {
-    if (Object.getPrototypeOf(this.constructor) === Abstract) {
-      throw new Error(`Abstract.constructor : ${this.name} is an abstract class. It cannot be instantiated.`);
-    }
     if (this.constructor === Abstract) {
       throw new Error('Abstract.constructor : Abstract class cannot be instantiated.');
+    }
+    if (Object.getPrototypeOf(this.constructor) === Abstract) {
+      throw new Error(`Abstract.constructor : ${this.name} is an abstract class. It cannot be instantiated.`);
     }
   }
 }
