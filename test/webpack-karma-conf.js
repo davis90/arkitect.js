ae3f220045c0ba2c326fc240fdc0079ab119d000
davis90 <davis_france@hotmail.com>
2019-01-21 02:20:55 +0100

    fix(karma unit tests) : fix target for webpack karma unit tests

--- test/webpack-karma-conf.js
+++ test/webpack-karma-conf.js
@@ -3,6 +3,7 @@ const merge = require('webpack-merge');
 const webpackBase = require('../webpack.config.js');
 
 module.exports = merge.smart(webpackBase, {
+  target: "web",
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
